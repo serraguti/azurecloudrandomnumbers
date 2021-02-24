5a264dd488599c7e0dc652dd5b3127ebd2c4ec0c
Serra <pacoserranox@gmail.com>
2021-02-24 12:59:53 +0100

    Add project files.

---
+++ WCFServiceWebRole1/IServiceRandomNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFServiceWebRole1
+{
+    [ServiceContract]
+    public interface IServiceRandomNumbers
+    {
+        [OperationContract]
+        List<int> GetRandomNumbers();
+    }
+}
---
+++ WCFServiceWebRole1/ServiceRandomNumbers.svc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Text;
+
+namespace WCFServiceWebRole1
+{
+    [AspNetCompatibilityRequirements(RequirementsMode =
+         AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ServiceRandomNumbers : IServiceRandomNumbers
+    {
+        public List<int> GetRandomNumbers()
+        {
+            List<int> numeros = new List<int>();
+            Random random = new Random();
+            for (int i = 1; i <= 10; i++)
+            {
+                int num = random.Next(1, 1000);
+                numeros.Add(num);
+            }
+            return numeros;
+        }
+    }
+}
